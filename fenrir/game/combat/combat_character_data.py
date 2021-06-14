017a9a998273468d4bbf93acd65a7c8c9a188e30
bcongressi <bcongressi@ufl.edu>
2021-06-14 13:00:15 -0400

    Added character getters/setters
    +get_ id/level/speed/hp/attack/is_enemy
    +update_ level/speed/hp/attack
    +documentation added

--- fenrir/game/combat/combat_character_data.py
+++ fenrir/game/combat/combat_character_data.py
@@ -1,10 +1,56 @@
+"""
+.. module:: combat_character_data
+  :synopsis: module for creating a new character in the combat scenes. Can be ally or enemy
+"""
+
 import math
 
 
 class CombatCharacterData:
-    def __init__(self, level, speed, hp, attack, enemy = False):
+    """Class representing individual characters, and their attributes for the combat scenes
+
+    :param id: given id value for the new character
+    :param level: level for the new character (can be updated later)
+    :param speed: speed/initiative value for the new character (can be updated later)
+    :param hp: hp for the new character (can be updated later)
+    :param attack: base attack value for new character (can be updated later)
+    :param enemy: boolean to determine if character is enemy. Defaults to false
+    """
+
+    def __init__(self, id, level, speed, hp, attack, enemy=False):
+        self._id = id
         self._level = level
         self._speed = speed
         self._hp = hp
         self._attack = attack
         self._enemy = enemy
+
+    def get_id(self):
+        return self._id
+
+    def get_level(self):
+        return self._level
+
+    def get_speed(self):
+        return self._speed
+
+    def get_hp(self):
+        return self._hp
+
+    def get_attack(self):
+        return self._attack
+
+    def get_is_enemy(self):
+        return self._enemy
+
+    def update_level(self, newLevel):
+        self._level = newLevel
+
+    def update_speed(self, newSpeed):
+        self._speed = newSpeed
+
+    def update_hp(self, newHp):
+        self._hp = newHp
+
+    def update_attack(self, newAttack):
+        self._attack = newAttack

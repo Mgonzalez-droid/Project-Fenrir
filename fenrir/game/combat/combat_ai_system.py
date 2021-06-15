b08637327bee63e8217ac79417e34f0e12d7fe39
bcongressi <bcongressi@ufl.edu>
2021-06-15 15:48:41 -0400

    Created combat_ai_system class
    +constructor
    +decide_where_to_move
    +decide_who_to_attack

---
+++ fenrir/game/combat/combat_ai_system.py
@@ -0,0 +1,12 @@
+import math
+
+
+class CombatAISystem:
+    def __init__(self, participants):
+        self._list_of_enemies = participants
+
+    def decide_who_to_attack(self):
+        print("Who to attack")
+
+    def decide_where_to_move(self):
+        print("Where to move")

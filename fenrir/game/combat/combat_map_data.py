03435c620de2263617e4c4abf3d4b3697f2a717e
Victor Sotomayor <vialso2012@gmail.com>
2021-06-18 13:47:30 -0400

    Uploaded combat_map_data.py since last commit missed it by accident

--- fenrir/game/combat/combat_map_data.py
+++ fenrir/game/combat/combat_map_data.py
@@ -1,174 +1,150 @@
 """
 .. module:: combat_map_data
-  :synopsis: module for creating tilesets and maps for combat scenes.
+  :synopsis: module for creating tilesets and reading maps for combat scenes.
 """
 
-import pygame
-import configparser
-
-
+"""
+:MAP_TILE_W: (int) holds standard width of tiles
+:MAP_TILE_H: (int) holds standard height of tiles
+"""
 MAP_TILE_W = 16
 MAP_TILE_H = 16
 
-class TileCache:
-    """Class to allow slicing of tilesets into tiles in list of lists
+class MapTile:
+    """
+    Class represents tiles that will be used to populate combat map lists
 
-        :param filename: (file) given tileset file to slice
-        :param w: (int) individual tile width
-        :param h: (int) individual tile height
+    :param t_type: (string) tells us what kind of tile it is, used to determine other attributes
+    :param x_coord, y_coord: (int) tells us the top left corner of the tile in x and y coordinates
 
-        Other non-param values:
-        :image: () loaded image from input file
-        :image_w: (int) width of the image
-        :image_h: (int) height of the image
-        :tile_map: (list of line) a list containing a list of lines which contains each rect/tile
-        :line: (list) a list containing each rect/tile in a row
-        :rect: () a tile and its coordinates
-        """
+    :wall: (boolean) tells us if the tile is a wall, if true also sets blocking to true
+    :blocking: (boolean) tells us if the tile blocks movement
+    :occupied: (boolean) tells us if there is a unit on the tile
+    :unit: (string) tells us the name/ID of the unit occupying the tile
+    """
 
-    """Load tilesets into our global cache"""
+    def __init__(self, t_type, x_coord, y_coord):
+        # Standard Types: ground, wall, blocking
+        self._t_type = t_type
+        # Coordinates are in increments of 16, top left corner of each tile
+        self._x_coord = x_coord
+        self._y_coord = y_coord
+        self._occupied = False
+        self._unit = ""
+        # Wall and blocking attributes to determine movement
+        if self._t_type == "wall":
+            self._wall = True
+            self._blocking = True
+        elif self._t_type == "blocking":
+            self._wall = False
+            self._blocking = True
+        else:
+            self._wall = False
+            self._blocking = False
 
-    def __init__(self, width=16, height=16):
-        self.width = width
-        self.height = height
-        self.cache = {}
+    def is_wall(self):
+        return self._wall
 
-    def __get_item__(self, filename):
-        """Returns table of tiles"""
-        key = (filename, self.width, self.height)
-        try:
-            return self.cache[key]
-        except KeyError:
-            tile_map = self._load_tile_map(filename, self.width, self.height)
-            self.cache[key] = tile_map
-            return tile_map
+    def is_blocking(self):
+        return self._blocking
 
-    def _load_tile_map(self, filename, width, height):
-        image = pygame.image.load(filename).convert()
-        image_w, image_h = image.get_size()
-        tile_map = []
-        for c_tile in range(0, image_w / width):
-            line = []
-            tile_map.append(line)
-            for r_tile in range(0, image_h / height):
-                rect = (c_tile * width, r_tile * height, width, height)
-                # subsurface returns tiles w/o creating copies in memory
-                line.append(image.subsurface(rect))
-        return tile_map
+    def is_occupied(self):
+        return self._occupied
 
-MAP_CACHE = TileCache(MAP_TILE_W, MAP_TILE_H)
+    def occupy(self, unit):
+        self._occupied = True
+        self._unit = unit
 
-class Level(object):
-    def load_file(self, filename="level_001.map"):
-        self.map = []
-        self.key = {}
-        parser = configparser.ConfigParser()
-        parser.read(filename)
-        self.tileset = parser.get("level", "tileset")
-        self.map = parser.get("level", "map").split("\n")
-        for sec in parser.sections():
-            if len(sec) == 1:
-                desc = dict(parser.items(sec))
-                self.key[sec] = desc
-        self.width = len(self.map[0])
-        self.height = len(self.map)
+    def unoccupy(self):
+        self._occupied = False
+        self._unit = ""
 
-    def get_tile(self, x, y):
-        try:
-            char = self.map[x][y]
-        except IndexError:
-            return {}
-        try:
-            return self.key[char]
-        except KeyError:
-            return {}
+    @property
+    def x_coord(self):
+        return self._x_coord
 
-    def get_bool(self, x, y, name):
-        val = self.get_tile(x, y).get(name)
-        return val in (True, 1, "true", "True", "yes", "Yes", "on", "On", "1")
+    @x_coord.setter
+    def x_coord(self, value):
+        self._x_coord = value
 
-    def is_wall(self, x, y):
-        return self.get_bool(x, y, "wall")
+    @property
+    def y_coord(self):
+        return self._y_coord
 
-    def is_blocking(self, x, y):
-        return self.get_bool(x, y, "block")
-
-    def render(self):
-        wall = self.is_wall
-        tiles = MAP_CACHE[self.tileset]
-        image = pygame.Surface((self.width * MAP_TILE_W, self.height * MAP_TILE_H))
-        overlays = {}
-        for map_y, line in enumerate(self.map):
-            for map_x, c in enumerate(line):
-                if wall(map_x, map_y):
-                    # Draw different tiles depending on neighbourhood
-                    if not wall(map_x, map_y + 1):
-                        if wall(map_x + 1, map_y) and wall(map_x - 1, map_y):
-                            tile = 0, 5
-                        elif wall(map_x + 1, map_y):
-                            tile = 0, 0
-                        elif wall(map_x - 1, map_y):
-                            tile = 0, 1
-                        else:
-                            tile = 0, 2
-                    else:
-                        if wall(map_x + 1, map_y + 1) and wall(map_x - 1, map_y + 1):
-                            tile = 0, 3
-                        elif wall(map_x + 1, map_y + 1):
-                            tile = 0, 11
-                        elif wall(map_x - 1, map_y + 1):
-                            tile = 0, 12
-                        else:
-                            tile = 0, 10
-                    # Add overlays if the wall may be obscuring something
-                    if not wall(map_x, map_y - 1):
-                        if wall(map_x + 1, map_y) and wall(map_x - 1, map_y):
-                            over = 1, 0
-                        elif wall(map_x + 1, map_y):
-                            over = 0, 0
-                        elif wall(map_x - 1, map_y):
-                            over = 2, 0
-                        else:
-                            over = 3, 0
-                        overlays[(map_x, map_y)] = tiles[over[0]][over[1]]
-                else:
-                    try:
-                        tile = self.key[c]['tile'].split(',')
-                        tile = int(tile[0]), int(tile[1])
-                    except (ValueError, KeyError):
-                        # Default to ground tile
-                        tile = 2, 2
-                tile_image = tiles[tile[0]][tile[1]]
-                image.blit(tile_image,
-                           (map_x * MAP_TILE_W, map_y * MAP_TILE_H))
-        return image, overlays
+    @y_coord.setter
+    def y_coord(self, value):
+        self._y_coord = value
 
 
-if __name__ == "__main__":
-    pygame.init()
+class MapData:
+    """
+        Class represents the map that will be used in combat
 
-    game_over = False
+        :param name: (string) file name associated with the map
+                              used to load .png map images and .txt map data
+        :param columns: (int) how many tiles in the vertical (height divided by 16)
+        :param rows: (int) how many tiles in the horizontal (width divided by 16)
+        :char_map: (string)  2D list of characters representing tiles, used to populate
+                        and define tilemap
+                        (we can use files associated with map images to populate this)
 
-    screen = pygame.display.set_mode((960, 540))
-    level = Level()
-    level.load_file("level_001.map")
-    clock = pygame.time.Clock()
+        :height: (int) height of the map .png (should be a multiple of 16)
+        :width: (int) width of the map .png (should be a multiple of 16)
+        :tilemap: (MapTile) 2D list of all the tiles on the map
 
-    background, overlay_dict = level.render()
-    overlays = pygame.sprite.RenderUpdates()
-    for (x, y), image in overlay_dict.iteritems():
-        overlay = pygame.sprite.Sprite(overlays)
-        overlay.image = image
-        overlay.rect = image.get_rect().move(x * 16, y * 16 - 16)
-    screen.blit(background, (0, 0))
+        Note: Each map should not be edited after it is created. Tilemap data should be taken from the object
+              and moved into a variable in the method that is running it so that the original map data isn't
+              altered
+    """
 
-    while not game_over:
-        overlays.draw(screen)
-        pygame.display.flip()
-        clock.tick(15)
-        for event in pygame.event.get():
-            if event.type == pygame.locals.QUIT:
-                game_over = True
-            elif event.type == pygame.locals.KEYDOWN:
-                pressed_key = event.key
+    def __init__(self, name, columns, rows, char_map):
+        # Name will be used to append .png or .txt to pull data from
+        self._name = name
+        # Number of tiles horizontally and vertically respectively
+        self._columns = columns
+        self._rows = rows
+        # Map should be a 2D List of strings (single chars)
+        self._char_map = char_map
+        # Dimensions based off of tile numbers
+        self._height = self._rows *  MAP_TILE_H
+        self._width = self._columns * MAP_TILE_W
+        # Tilemap population and definition
+        self._tilemap = []
+        for i in range(self._rows):
+            # Create a temp list to append to our first list so we can make it 2D
+            _temp_list = []
+            # Redundant, can be removed if necessary, but makes sure temp list is EMPTY before appending
+            _temp_list.clear()
+            for j in range(self._columns):
+                if self._char_map[i][j] == ".":
+                    _temp_list.append(MapTile("ground", i * MAP_TILE_W, j * MAP_TILE_H))
+                elif self._char_map[i][j] == "#":
+                    _temp_list.append(MapTile("wall", i * MAP_TILE_W, j * MAP_TILE_H))
+                elif self._char_map[i][j] == "~":
+                    _temp_list.append(MapTile("blocking", i * MAP_TILE_W, j * MAP_TILE_H))
+            # Append our columns to each row
+            self._tilemap.append(_temp_list)
 
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def columns(self):
+        return self._columns
+
+    @property
+    def rows(self):
+        return self._rows
+
+    @property
+    def height(self):
+        return self._height
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def tilemap(self):
+        return self._tilemap

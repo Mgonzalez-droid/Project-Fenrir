51020517fbc0fe83e58ded08c6f6c35e6544c5ea
Victor Sotomayor <vialso2012@gmail.com>
2021-06-22 15:14:13 -0400

    Changed combat map size and txt to accomodate Note: 2D List of chars must be 23 width x 13 height, PNG must be 960 x 540 Note: Rightmost 40 pixels and Bottommost 20 pixels of PNG aren't used by the combat grid

--- fenrir/game/combat/combat_map_data.py
+++ fenrir/game/combat/combat_map_data.py
@@ -8,8 +8,8 @@ from fenrir.common.config import PATH_TO_RESOURCES
 :MAP_TILE_W: (int) holds standard width of tiles
 :MAP_TILE_H: (int) holds standard height of tiles
 """
-MAP_TILE_W = 16
-MAP_TILE_H = 16
+MAP_TILE_W = 40
+MAP_TILE_H = 40
 
 
 class MapTile:
@@ -28,7 +28,7 @@ class MapTile:
     def __init__(self, t_type, x_coord, y_coord):
         # Standard Types: ground, wall, blocking
         self._t_type = t_type
-        # Coordinates are in increments of 16, top left corner of each tile
+        # Coordinates are in increments of 40, top left corner of each tile
         self._x_coord = x_coord
         self._y_coord = y_coord
         self._occupied = False
@@ -87,14 +87,14 @@ class MapData:
 
         :param name: (string) file name associated with the map
                               used to load .png map images and .txt map data
-        :param columns: (int) how many tiles in the vertical (height divided by 16)
-        :param rows: (int) how many tiles in the horizontal (width divided by 16)
+        :param columns: (int) how many tiles in the vertical (height divided by 40)
+        :param rows: (int) how many tiles in the horizontal (width divided by 40)
         :char_map: (string)  2D list of characters representing tiles, used to populate
                         and define tilemap
                         (we can use files associated with map images to populate this)
 
-        :height: (int) height of the map .png (should be a multiple of 16)
-        :width: (int) width of the map .png (should be a multiple of 16)
+        :height: (int) height of the map .png (should be a multiple of 40)
+        :width: (int) width of the map .png (should be a multiple of 40)
         :tilemap: (MapTile) 2D list of all the tiles on the map
 
         Note: Each map should not be edited after it is created. Tilemap data should be taken from the object
@@ -109,11 +109,13 @@ class MapData:
         self._columns = columns
         self._rows = rows
         # Map should be a 2D List of strings (single chars)
+        # Char map txt file MUST BE 23w x 13h!
         self._char_map = self.load_charmap()
         # Dimensions based off of tile numbers
         self._height = self._rows * MAP_TILE_H
         self._width = self._columns * MAP_TILE_W
         # Tilemap population and definition
+        # Tilemap PNG MUST be 960 x 540!
         self._tilemap = []
         for i in range(self._rows):
             # Create a temp list to append to our first list so we can make it 2D

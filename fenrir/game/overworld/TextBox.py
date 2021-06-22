fe14d346fa1ff82cd3d885df643c6056b4372267
Robrverde <61200010+Robrverde@users.noreply.github.com>
2021-06-22 16:49:25 -0400

    Added function to create text box were the playes can pick an option

--- fenrir/game/overworld/TextBox.py
+++ fenrir/game/overworld/TextBox.py
@@ -30,19 +30,33 @@ class TextBox:
         self.screen.blit(text_box, (text_box_x_pos, text_box_y_pos))
 
     # Draw text
-    def draw_dialogue(self, text):
-
+    def draw_dialogue(self, text, size, x, y):
         # The current text box can take 72 characters per line
         # keys = pygame.key.get_pressed()
         chars_per_line = 71
         lines = [text[i:i + chars_per_line] for i in range(0, len(text), chars_per_line)]
-        font = pygame.font.Font(None, 24)
+        font = pygame.font.Font(None, size)
+
         # line_height is used to make sure the next line of text goes below the one before it
         line_height = 0
 
         for line in lines:
             dialogue = font.render(line, True, (0, 0, 0))
-            self.screen.blit(dialogue, (200, 407 - line_height))
+            self.screen.blit(dialogue, (x, y - line_height))
             # Next line of text will be below the line before it
             line_height = line_height - 16
+
+    def draw_options(self, question, options, size, x, y):
+
+        # Draw question inside text box
+        self.draw_dialogue(question, size, x, y)
+
+        # Draw the available options in the screen
+        x = x + 35
+        y = y + 30
+        line_height = 0
+        for option in options:
+
+            self.draw_dialogue(option, size, x, y - line_height)
+            line_height = line_height - 25
 

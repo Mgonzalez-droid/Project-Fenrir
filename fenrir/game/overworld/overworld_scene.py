be0d438b1524df60965cd57d6c7edcd990e75c42
michel <gonzalezmike96@gmail.com>
2021-06-23 16:06:55 -0400

    Added new imagery for vertical slice to meet requirements. Added new Controls menu and changed some code to switch between menus

--- fenrir/game/overworld/overworld_scene.py
+++ fenrir/game/overworld/overworld_scene.py
@@ -16,31 +16,52 @@ class OverworldScene(Scene):
 
     def __init__(self, screen):
         super().__init__(screen)
-        self.background = pygame.image.load(os.path.join(PATH_TO_RESOURCES, "demo_overworld.png"))
-        self.hero = character(0, 0, pygame.image.load(os.path.join(PATH_TO_RESOURCES, "gabe-idle-run 2.png")))
-        self.npc = character(0, 0, pygame.image.load(os.path.join("fenrir/resources/chars/sensei/sensei.png")))
+        self.background = pygame.image.load(os.path.join(PATH_TO_RESOURCES, "overworld_resized_controls.png"))
+        self.hero = character(560, 170, pygame.image.load(os.path.join(PATH_TO_RESOURCES, "gabe_resized.png")))
+        self.npc = character(890, 230, pygame.image.load(os.path.join(PATH_TO_RESOURCES, "Resized_Sensei.png")))
+        self.in_controls = False
+        self.show_characters = True
 
     def handle_event(self, event):
         """Example event handling. Will return to main menu if you
 
         """
+        ## Track Movement ##
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.hero.y -= 10
+        if keys[pygame.K_s]:
+            self.hero.y += 10
+        if keys[pygame.K_a]:
+            self.hero.x -= 10
+        if keys[pygame.K_d]:
+            self.hero.x += 10
+        '''
+        if keys[pygame.K_UP]:
+            self.hero.y -= 10
+        if keys[pygame.K_DOWN]:
+            self.hero.y += 10
+        if keys[pygame.K_LEFT]:
+            self.hero.x -= 10
+        if keys[pygame.K_RIGHT]:
+            self.hero.x += 10
+        '''
+
+        ## Track Interaction ##
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER:
                 self.switch_to_scene(menuscene.MainMenuScene(self.screen))
             if event.key == pygame.K_q:
-                print("Entered menu")
-            if event.key == pygame.K_w or event.key == pygame.K_UP:
-                #print("+10 in y direction")
-                self.hero.y += 10
-            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                #print("-10 in y direction")
-                self.hero.y -= 10
-            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                #print("-10 in x direction")
-                self.hero.x -= 10
-            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                #print("+10 in x direction")
-                self.hero.x += 10
+                if self.in_controls:
+                    self.background = pygame.image.load(
+                        os.path.join(PATH_TO_RESOURCES, "overworld_resized_controls.png"))
+                    self.in_controls = False
+                    self.show_characters = True
+                else:
+                    self.background = pygame.image.load(os.path.join(PATH_TO_RESOURCES, "Simple_Control_menu.png"))
+                    self.in_controls = True
+                    self.show_characters = False
+
             if event.key == pygame.K_SPACE:
                 print("scan for interaction")
                 # try to interact with object
@@ -55,8 +76,9 @@ class OverworldScene(Scene):
         print(self.hero.y)
         self.screen.fill(Colors.WHITE.value)
         self.screen.blit(self.background, (0, 0))
-        self.screen.blit(self.hero.sprite, (self.hero.x, self.hero.y))
-        self.screen.blit(self.npc.sprite, (self.npc.x, self.npc.y))
+        if self.show_characters:
+            self.screen.blit(self.hero.sprite, (self.hero.x, self.hero.y))
+            self.screen.blit(self.npc.sprite, (self.npc.x, self.npc.y))
 
     def update(self):
         pass

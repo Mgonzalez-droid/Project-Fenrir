21fa6321de18c615b855728cbb1e46294bea33c8
Robrverde <61200010+Robrverde@users.noreply.github.com>
2021-06-26 12:40:29 -0400

    Player can now interact with the npc and a textbox will appear, screen boundaries are working, movement is working.

--- fenrir/game/overworld/overworld_scene.py
+++ fenrir/game/overworld/overworld_scene.py
@@ -2,15 +2,14 @@
 
 """
 import os
-
-from fenrir.game.overworld.overworld_npc import overworld_npc as character
-from fenrir.game.overworld.overworld_npc_animated import  overworld_npc_animated as character_animated
-
 import pygame
-
 import fenrir.game.menu.menu_scene as menuscene
 from fenrir.common.scene import Scene
 from fenrir.common.config import Colors, PATH_TO_RESOURCES
+from fenrir.common.TextBox import TextBox
+from fenrir.game.overworld.overworld_npc import overworld_npc as character
+from fenrir.game.overworld.overworld_npc_animated import overworld_npc_animated as character_animated
+from fenrir.game.overworld.overworld_boundaries import Boundaries
 
 
 class OverworldScene(Scene):
@@ -24,82 +23,95 @@ class OverworldScene(Scene):
 
         self.npc = character(890, 230, os.path.join(PATH_TO_RESOURCES, "Resized_Sensei.png"))
         self.exclamation_mark = character(860, 170, os.path.join(PATH_TO_RESOURCES, "exclamation.png"))
-        self.exclamation_mark.sprite = pygame.transform.scale(self.exclamation_mark.sprite, (100,100))
-        self.in_controls = False
+        self.exclamation_mark.sprite = pygame.transform.scale(self.exclamation_mark.sprite, (100, 100))
+        self.show_controls = False
         self.show_characters = True
         self.combat_phase = False
         self.show_interaction = False
+        self.show_textbox = False
 
     def handle_event(self, event):
         """Example event handling. Will return to main menu if you
 
         """
-        # Track Movement
+        # TRACK MOVEMENT
+        # Boundaries class prevent the player character to move outside the current window
+        boundaries = Boundaries(self.screen, self.hero)
+
+        # Player check player movement for up (w), down (s), left (a), right (d)
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w] and not self.in_controls and not self.combat_phase:
-            self.hero.y -= 10
-            self.hero.adjust_movement()
+        if not self.show_controls and not self.show_textbox:
+            if keys[pygame.K_w]:
+                self.hero.y = boundaries.collision_up()  # Check if player hits top of window
+                self.hero.adjust_movement()
+            if keys[pygame.K_s]:
+                self.hero.y = boundaries.collision_down()  # Check if player hits bottom of window
+                self.hero.adjust_movement()
+            if keys[pygame.K_a]:
+                self.hero.x = boundaries.collision_left()  # Check if player hits left of window
+                self.hero.adjust_movement()
+            if keys[pygame.K_d]:
+                self.hero.x = boundaries.collision_right()  # Check if player hits right of window
+                self.hero.adjust_movement()
 
-        if keys[pygame.K_s] and not self.in_controls and not self.combat_phase:
-            self.hero.y += 10
-            self.hero.adjust_movement()
-
-        if keys[pygame.K_a] and not self.in_controls and not self.combat_phase:
-            self.hero.x -= 10
-            self.hero.adjust_movement()
-
-        if keys[pygame.K_d] and not self.in_controls and not self.combat_phase:
-            self.hero.x += 10
-            #self.hero.move()
-            #self.hero.animate = True
-            #self.hero.play_animation()
-            self.hero.adjust_movement()
-
-
-        #PLACE CODE HERE FOR MENU SELECTION
-        '''
-        if keys[pygame.K_SPACE] and self.combat_phase
-            this will trigger combat
-        '''
-
-        if (self.hero.x >= 820 and self.hero.x <=900) and (self.hero.y >= 190 and self.hero.y <= 250):
-            #Show exclamation mark#
+        # Check of collision
+        if (820 <= self.hero.x <= 900) and (190 <= self.hero.y <= 250):
+            # Show exclamation mark
             self.show_interaction = True
 
-        ## Track Interaction ##
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER:
+        # TRACK INTERACTION
+        if event.type == pygame.KEYDOWN:  # Press Enter or Esc to go back to the Main Menu
+            if event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER or event.key == pygame.K_ESCAPE:
                 self.switch_to_scene(menuscene.MainMenuScene(self.screen))
-            if event.key == pygame.K_q:
-                if self.in_controls:
+
+            if event.key == pygame.K_q:  # Press q to open/close controls menu
+                if self.show_controls:
                     self.background = pygame.image.load(
                         os.path.join(PATH_TO_RESOURCES, "overworld_resized_controls.png"))
-                    self.in_controls = False
+                    self.show_controls = False
                     self.show_characters = True
                 else:
                     self.background = pygame.image.load(os.path.join(PATH_TO_RESOURCES, "Simple_Control_menu.png"))
-                    self.in_controls = True
+                    self.show_controls = True
                     self.show_characters = False
 
-            if event.key == pygame.K_SPACE and not self.in_controls:
-                #CODE TO ENTER COMBAT PHASE#
-                if (self.hero.x >= 820 and self.hero.x <=900) and (self.hero.y >= 190 and self.hero.y <= 250):
-                    print("Entering combat phase, locking player controls")
-                    #STOP CHARACTER MOVEMENT#
-                    self.combat_phase = True
+            if event.key == pygame.K_SPACE and not self.show_controls:  # Checks if the space bar is pressed
+                # Check for collision
+                if (820 <= self.hero.x <= 900) and (190 <= self.hero.y <= 250):
+                    # if text box is displayed, stop characters movements
+                    self.show_textbox = True
+
+            # Select options from the text box
+            if event.key == pygame.K_1 and self.show_textbox:
+                print("Going to combat phase")
+            if event.key == pygame.K_2 and self.show_textbox:
+                self.show_textbox = False
 
     def render(self):
-        #print("hero coordinates: ", end='')
-        #print(self.hero.x, end='')
-        #print(self.hero.y)
+        # print("hero coordinates: ", end='')
+        # print(self.hero.x, end='')
+        # print(self.hero.y)
         self.screen.fill(Colors.WHITE.value)
         self.screen.blit(self.background, (0, 0))
         self.hero.play_animation()
-        if self.show_characters:
+
+        if self.show_characters:  # Display hero and npcs
             self.screen.blit(self.hero.sprite, (self.hero.x, self.hero.y))
             self.screen.blit(self.npc.sprite, (self.npc.x, self.npc.y))
-        if self.show_interaction:
+
+        if self.show_interaction:  # Show if you can interact with and npc
             self.screen.blit(self.exclamation_mark.sprite, (self.exclamation_mark.x, self.exclamation_mark.y))
+
+        if self.show_textbox:  # Draw Text box
+            textbox = TextBox(self.screen)
+            TextBox.load_textbox(textbox)
+            options = ["[1] Yes, I am ready to go to the combat phase",
+                       "[2] No, I want to keep walking here"]
+            size = 24
+            x = 200
+            y = 397
+            # Text box were the user must pick and option
+            TextBox.draw_options(textbox, "Hello Gabe, do you wanna go to the combat phase?", options, size, x, y)
 
     def update(self):
         pass

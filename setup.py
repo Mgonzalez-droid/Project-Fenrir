041743ad878d3447f01aa914d2dbbbba67bb3e5a
Bryan <bryangators510@gmail.com>
2021-06-28 12:48:18 -0400

    updated author names to be a list instead of one string in setup.py

--- setup.py
+++ setup.py
@@ -9,18 +9,12 @@ def read(name):
 setup(
     name="Project Fenrir",
     version="0.0.1",
-    author=["Victor Sotomayor Monroy, Michel Gonzalez, Barry Congressi, Bryan Kristofferson, Roberto Rafael Edde Verde"],
+    author=["Victor Sotomayor Monroy", "Michel Gonzalez", "Barry Congressi", "Bryan Kristofferson", "Roberto Rafael Edde Verde"],
     author_email="victor@ufl.edu",
     description="A fantasy turn-based role-playing game",
     long_description=read('README.md'),
     lisence="MIT",
     url="https://github.com/Mgonzalez-droid/Project-Fenrir",
     packages=find_packages(),
-    install_requires=["pygame"],
-    entry_points={
-        "console_scripts":
-            [
-                "play_fenrir=fenrir.app:run"
-            ]
-    }
+    install_requires=["pygame"]
 )

50d078572b8e3b6755f844fd2e752a727808eb0e
michel <gonzalezmike96@gmail.com>
2021-07-13 15:41:02 -0400

    made changes to overworld collisions to include colision between characters and obstacles. Inlcuded an additional class to hold the attributes for an in game obstacle. This will handle collision between characters and untraversable areas such as water, flower patches, etc.

--- fenrir/game/overworld/overworld_collisions.py
+++ fenrir/game/overworld/overworld_collisions.py
@@ -10,9 +10,19 @@ class Collision:
         character = pygame.Rect(npc.x, npc.y, npc.sprite.get_width(), npc.sprite.get_height())
         return character
 
-    def check_collisions(self, hero, npc):
+    @staticmethod
+    def make_obstacle_rect(obstacle):
+        obstacle = pygame.Rect(obstacle.x, obstacle.y, obstacle.get_width(), obstacle.get_height())
+        return obstacle
+
+    def check_collisions(self, hero, npc, obstacles=list()):
         hero_rect = self.make_rect(hero)
         npc_rect = self.make_rect(npc)
+        obstacle_rects = list()
+        if obstacles:
+            # There are obstacles to consider
+            for obstacle in obstacles:
+                obstacle_rects.append(self.make_obstacle_rect(obstacle))
 
         if hero_rect.colliderect(npc_rect):
             if abs(npc_rect.top - hero_rect.bottom) < self.__tolerance:
@@ -23,5 +33,16 @@ class Collision:
                 return True
             if abs(npc_rect.left - hero_rect.right) < self.__tolerance:
                 return True
+
+        for obstacle_rect in obstacle_rects:
+            if hero_rect.colliderect(obstacle_rect):
+                if abs(obstacle_rect.top - hero_rect.bottom) < self.__tolerance:
+                    return True
+                if abs(obstacle_rect.bottom - hero_rect.top) < self.__tolerance:
+                    return True
+                if abs(obstacle_rect.right - hero_rect.left) < self.__tolerance:
+                    return True
+                if abs(obstacle_rect.left - hero_rect.right) < self.__tolerance:
+                    return True
 
         return False
---
+++ fenrir/game/overworld/overworld_obstacle.py
@@ -0,0 +1,38 @@
+class overworld_obstacle:
+    def __init__(self, x, y, width, height):
+        self.__x = x
+        self.__y = y
+        self.__width = width
+        self.__height = height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        self.__height = height

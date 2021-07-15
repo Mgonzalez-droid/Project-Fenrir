e6326b209ac148fc04ca20fb458ae2f189e3d9c7
Robrverde <61200010+Robrverde@users.noreply.github.com>
2021-07-15 15:35:33 -0400

    Improved display_heroes function from inventory.py class

--- fenrir/game/overworld/inventory.py
+++ fenrir/game/overworld/inventory.py
@@ -17,7 +17,6 @@ class Inventory:
         self.__inventory_tile_y = [187, 298]
         self.__inventory_tile_pos = [0, 0]
         self.swapping = False
-        self.adding = False
 
     # Display inventory background image box, character tiles and instruction text
     def display_inventory(self):
@@ -29,33 +28,38 @@ class Inventory:
         self.textbox.draw_dialogue("Press space bar to add/remove selected", 24, 260, 360)
         self.textbox.draw_dialogue(" hero from current party.", 24, 255, 390)
 
-    # Display heroes sprites inside inventory tiles
     def display_heroes(self, party, heroes):
+
+        # Set position were characters sprite will be displayed for the party and heroes sections
         party_x = 160
         party_y = 95
-
         heroes_x = 142
         heroes_y = 206
 
+        self.display_section(party_x, party_y, party, 0)
+        self.display_section(heroes_x, heroes_y, heroes, 1)
+
+    # Display heroes sprites inside inventory tiles
+    def display_section(self, x, y, section, index):
+
+        displayed = []
+
         # Display characters in the current party section
+        for i in range(len(section)):
+            self.textbox.load_image(x, y, 350, 250, section[i][1])
+            x += 40
+            displayed.append(True)
 
-        for i in range(len(party)):
-            self.textbox.load_image(party_x, party_y, 350, 250, party[i][1])
-            party_x += 40
-            self.party_displayed[i] = True
+        # Set tiles with no characters to false
+        if index == 0:  # Party section
+            for i in range(len(section), len(self.party_displayed)):
+                displayed.append(False)
+            self.party_displayed = displayed
 
-        # Set tiles with no characters to 0 again
-        for i in range(len(party), len(self.party_displayed)):
-            self.party_displayed[i] = False
-
-        # Display characters in the heroes section
-        for j in range(len(heroes)):
-            self.textbox.load_image(heroes_x, heroes_y, 350, 250, heroes[j][1])
-            heroes_x += 40
-            self.heroes_displayed[j] = True
-
-        for j in range(len(heroes), len(self.heroes_displayed)):
-            self.heroes_displayed[j] = False
+        else:  # Heroes section
+            for i in range(len(section), len(self.heroes_displayed)):
+                displayed.append(False)
+            self.heroes_displayed = displayed
 
     # Change the position of the tile the user is selecting
     def character_selection(self, index, tiles_num):
@@ -73,27 +77,18 @@ class Inventory:
     def display_selection(self, index):
         self.textbox.load_image(self.tile_x[index], self.tile_y[index], 35, 39, "UI/generic-rpg-ui-inventario01.png")
 
-    # Swap characters between current party and heroes section
+    # Swap characters in current party to the one selected in the heroes section
     def swap_characters(self, party_index, hero_index):
 
-        temp = self.party[int(party_index)]
         party_hero = self.heroes[int(hero_index)]
-        saved_hero = temp
-        return party_hero, saved_hero
+        return party_hero
 
     def add_to_party(self, hero):
 
         self.party.append(hero)
-        self.heroes.remove(hero)
+        # self.heroes.remove(hero)
 
         return self.party, self.heroes
-
-    def add_to_heroes(self, hero):
-
-        self.heroes.append(hero)
-        self.party.remove(hero)
-
-        return self.heroes, self.party
 
     @property
     def tile_x(self):

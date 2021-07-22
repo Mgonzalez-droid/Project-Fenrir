29f24476bfa1720e0ade4539807e4e483dbc7b3a
bcongressi <bcongressi@ufl.edu>
2021-07-22 01:24:29 -0400

    Updated ai to remove all current known bugs

--- fenrir/game/combat/combat_ai_system.py
+++ fenrir/game/combat/combat_ai_system.py
@@ -34,172 +34,171 @@ class CombatAISystem:
     """
 
     def __init__(self, participants, currentParticipant, nodeTree, mapData):
-        self._list_of_enemies = participants
-        self._me = currentParticipant
+        self.list_of_characters = participants
+        self.me = currentParticipant
         self._nodeTree = nodeTree
         self._copyOfMapData = mapData
-        self._myX = int((self._me.xpos - 30) / 60)
-        self._myY = int((self._me.ypos - 30) / 60)
+        self.myX = int((self.me.xpos - 30) / 60)
+        self.myY = int((self.me.ypos - 30) / 60)
+        self.enemy = None
+        self.enemyValue = 0
+        self.enemyX = 0
+        self.enemyY = 0
+        self.estimatedEnemyDistance = 0
+        self.enemyPathDistance = 0
+        self.enemyNextToMe = False
+        self.startNode = None
+        self.endNode = None
+        self.listToSearch = None
         self._goalX = None
         self._goalY = None
-        self._targetX = None
-        self._targetY = None
-        self._target = None
-        self._targetNode = None
-        self._opponentScore = 0
-        self._targetDistance = 0
-        self._targetNextToMe = False
-        self._targetNodeLessThanTargetPosition = False
-        self._openList = []
-        self._closedList = []
 
     def decide_who_to_attack(self):
-        """A* based function to decide on what character to attack. Based on distance.
+        """Uses distance from each target and type checking to decide which target should be the focus this turn.
         """
-        for i in self._list_of_enemies:
-            if not i.get_is_enemy() and i.hp > 0:
-                distanceX = abs(self._myX - ((i.xpos - 30) / 60))
-                distanceY = abs(self._myY - ((i.ypos - 30) / 60))
-                totalDist = distanceX + distanceY
-                enemyValue = totalDist
+        for potentialTarget in self.list_of_characters:
+            if not potentialTarget.get_is_enemy() and potentialTarget.hp > 0:
+                # Estimate the distance to this target
+                potentialTargetX = ((potentialTarget.xpos - 30) / 60)
+                potentialTargetY = ((potentialTarget.ypos - 30) / 60)
+                estimatedTotalDist = abs(self.myX - potentialTargetX) + abs(self.myY - potentialTargetY)
 
-                if totalDist == 1:  # an enemy is next to a AI!
-                    self._targetNextToMe = True
-                    self._target = i  # set target and return
+                # Check if enemy is next to me
+                if estimatedTotalDist == 1:
+                    self.enemyNextToMe = True
+                    self.enemy = potentialTarget
                     return
-                elif totalDist <= self._me.attack_range:  # enemy is not next to AI but is in attack range
-                    enemyValue += 0  # best option
-                elif totalDist <= self._me.attack_range + self._me.move_range:  # enemy is in range if AI moves closer
-                    enemyValue -= (self._me.move_range - 1)  # good option
-                else:  # enemy is out of range. AI will have to move only
-                    enemyValue += 2  # poor option
 
-                if self._target is None or enemyValue < self._opponentScore:  # current best target for the AI
-                    self._target = i
-                    self._targetX = int((i.xpos - 30) / 60)
-                    self._targetY = int((i.ypos - 30) / 60)
-                    self._targetDistance = totalDist
-                    self._opponentScore = enemyValue
+                # The value of the target is measured in distance. Each point added increase the range the ai can look
+                # for a better target in the list
+                enemyTargetValue = estimatedTotalDist
+
+                # If this target is like me then they aren't a great target so we give extra buffer to look elsewhere
+                if potentialTarget.get_type() == self.me.get_type():
+                    enemyTargetValue += 5
+
+                # If this target is within range if I move first then they are a better target
+                if estimatedTotalDist <= self.me.attack_range + self.me.move_range:
+                    enemyTargetValue -= 2
+                else:
+                    # If this target is too far to attack this turn then they aren't a great target
+                    enemyTargetValue += 2
+
+                # If there is not a set target yet or if this target is better than the previous then set this as enemy
+                if self.enemy is None or enemyTargetValue < self.enemyValue:
+                    self.enemy = potentialTarget
+                    self.enemyX = potentialTargetX
+                    self.enemyY = potentialTargetY
+                    self.estimatedEnemyDistance = estimatedTotalDist
+                    self.enemyValue = enemyTargetValue
 
     def build_path_to_target(self):
         """Function to decide where to move the ai on the map. Returns x Coord to move to, y Coord to move to, target id
         to attack this turn. based on A*
         """
-        # make list to search and list already searched
-        openList = []
-        closedList = []
-
-        # find the first node and add it to the list to search. Set node's value to hold the distance to target
+        self.listToSearch = []
+        # Reset all the nodes and add the start node to the list
         for node in self._nodeTree:
             node.clear_data()
-            if node.get_xPos() == self._myX and node.get_yPos() == self._myY:
-                openList.append(node)
-                node.set_totalDistance(self._targetDistance)
+            if node.get_xPos() == self.myX and node.get_yPos() == self.myY:
+                self.startNode = node
+                self.startNode.set_distanceToGoal(self.enemyX, self.enemyY)
+                self.listToSearch.append(node)
 
-        # as long as there are nodes to search keep looping
-
-        while len(openList) > 0:
-            # pick the tile closest to the target from the openList
-            bestTile = openList[0]
-            bestTileIndex = 0
+        while len(self.listToSearch) > 0:
+            currentNode = self.listToSearch[0]
+            currentNodeIndex = 0
             counter = 0
-            for tile in openList:
-                if bestTile.get_totalDistance() > tile.get_totalDistance():
-                    bestTile = tile
-                    bestTileIndex = counter
+
+            # Find the node with the smallest distance to the goal
+            for tile in self.listToSearch:
+                if tile.get_finalCost() < currentNode.get_finalCost():
+                    currentNode = tile
+                    currentNodeIndex = counter
                 counter += 1
-            currentTile = bestTile
+            self.listToSearch.pop(currentNodeIndex)
 
-            openList.pop(bestTileIndex)
-            closedList.append(currentTile)
-
-            # check if the current tile is the goal
-            if currentTile.get_xPos() == self._targetX and currentTile.get_yPos() == self._targetY:
-                self._targetNode = currentTile
-                return  # found the end
-            # check if the current tile is close enough to attack from
-            if currentTile.get_totalDistance() - currentTile.get_distanceFromStart() <= self._me.attack_range:
-                self._targetNode = currentTile
-                self._targetNodeLessThanTargetPosition = True
-                self._targetDistance = currentTile.get_distanceFromStart() + 1
+            # Found the enemy position in the nodeTree
+            if currentNode.get_xPos() == self.enemyX and currentNode.get_yPos() == self.enemyY:
+                self.endNode = currentNode
+                self.set_enemy_path_distance()
                 return
 
-            # loop and add all of the current node's neighbors to the list to search if not occupied
-            for neighbor in currentTile.get_neighbors():
+            if currentNode.get_distanceToGoal() <= self.me.attack_range:
+                self.endNode = currentNode
+                self.set_enemy_path_distance()
+                return
+
+            # check if the node neighbors have been seen before if so check best path, if not add to list
+            for neighbor in currentNode.get_neighbors():
+                # TODO determine what to do about occupied spaces
                 if self._copyOfMapData.tilemap[neighbor.get_yPos()][neighbor.get_xPos()].is_occupied:
-                    continue
-                alreadySearched = False
-                # loop through list of nodes we have searched to make sure we haven't seen this node before
-                for tileWeHaveSeen in closedList:
-                    # if the node we want to add matches one in the already search list, then break
-                    if neighbor.get_xPos() == tileWeHaveSeen.get_xPos() and neighbor.get_yPos() == tileWeHaveSeen.get_yPos():
-                        alreadySearched = True
-                        break
-                # if this node isn't new move to the next node in the list of neighbors
-                if alreadySearched:
-                    continue
+                    if neighbor.get_xPos() != self.enemyX and neighbor.get_yPos() != self.enemyY:
+                        continue
+                nodeGivenCost = neighbor.calculate_givenCost(currentNode.get_givenCost())
+                if neighbor.get_parent() is None and neighbor != self.startNode:
+                    neighbor.set_parent(currentNode)
+                    neighbor.set_givenCost(nodeGivenCost)
+                    neighbor.set_distanceToGoal(self.enemyX, self.enemyY)
+                    neighbor.set_finalCost()
+                    self.listToSearch.append(neighbor)
+                elif neighbor.get_parent() is not None:
+                    if nodeGivenCost < neighbor.get_givenCost():
+                        neighbor.set_givenCost(nodeGivenCost)
+                        neighbor.set_parent(currentNode)
+                        neighbor.set_finalCost()
 
-                # if node is new to search, calculate the length traveled so far, the distance to the end and set the node values
-                distanceX = abs(neighbor.get_xPos() - self._targetX)
-                distanceY = abs(neighbor.get_yPos() - self._targetY)
-                totalDist = distanceX + distanceY + currentTile.get_distanceFromStart() + 1
+    def set_enemy_path_distance(self):
+        counter = 0
+        currentNode = self.endNode
+        while currentNode.get_parent() is not None:
+            currentNode = currentNode.get_parent()
+            counter += 1
+        self.enemyPathDistance = counter
 
-                # check if the neighbor is in the open list. if so is this a better path?
-                inOpenList = False
-                for openNeighbor in openList:
-                    if openNeighbor.get_xPos() == neighbor.get_xPos() and openNeighbor.get_yPos() == neighbor.get_yPos():
-                        inOpenList = True
-                        if openNeighbor.get_totalDistance() > totalDist:
-                            neighbor.set_distanceFromStart(currentTile.get_distanceFromStart() + 1)
-                            neighbor.set_totalDistance(totalDist)
-                            neighbor.set_parent(currentTile)
-                        else:
-                            break
-                if not inOpenList:
-                    neighbor.set_distanceFromStart(currentTile.get_distanceFromStart() + 1)
-                    neighbor.set_totalDistance(totalDist)
-                    neighbor.set_parent(currentTile)
-                    openList.append(neighbor)
-
-    def set_ai_goal_position(self, numberOfTilesToMove):
-        currentTile = self._targetNode
-        if self._targetNode.get_xPos() == self._targetX and self._targetNode.get_yPos() == self._targetY:
-            numberOfTilesToMove -= 1
-            for i in range(self._targetDistance - 1 - numberOfTilesToMove):
-                currentTile = currentTile.get_parent()
-            self._goalX = (currentTile.get_xPos() * 60) + 30
-            self._goalY = (currentTile.get_yPos() * 60) + 30
-        elif self._targetNodeLessThanTargetPosition:
-            if self._targetDistance - 1 < numberOfTilesToMove:
-                self._goalX = (self._targetNode.get_xPos() * 60) + 30
-                self._goalY = (self._targetNode.get_yPos() * 60) + 30
+    def set_ai_goal_position(self, numberOfNodesToMove):
+        currentNode = self.endNode
+        if self.endNode.get_xPos() == self.enemyX and self.endNode.get_yPos() == self.enemyY:
+            # Stop next to enemy
+            numberOfNodesToMove -= 1
+            for _ in range(self.enemyPathDistance - numberOfNodesToMove):
+                currentNode = currentNode.get_parent()
+            self._goalX = (currentNode.get_xPos() * 60) + 30
+            self._goalY = (currentNode.get_yPos() * 60) + 30
+        else:
+            if self.enemyPathDistance < numberOfNodesToMove:
+                self._goalX = (self.endNode.get_xPos() * 60) + 30
+                self._goalY = (self.endNode.get_yPos() * 60) + 30
             else:
-                for i in range(self._targetDistance - 1 - numberOfTilesToMove):
-                    currentTile = currentTile.get_parent()
-                self._goalX = (currentTile.get_xPos() * 60) + 30
-                self._goalY = (currentTile.get_yPos() * 60) + 30
+                for _ in range(self.enemyPathDistance - numberOfNodesToMove):
+                    currentNode = currentNode.get_parent()
+                self._goalX = (currentNode.get_xPos() * 60) + 30
+                self._goalY = (currentNode.get_yPos() * 60) + 30
 
     def decide_ai_action(self):
         """Function decides if ai should only attack (next to enemy already), move twice (no enemy in range), or move then
         attack. Returns desired ai xcoord, ycoord, and target id"""
         self.decide_who_to_attack()
-        if self._target is None:
+
+        # If no enemies left to attack return all None to end game
+        if self.enemy is None:
             return None, None, None
-        if self._me.get_type() == 'mage':
-            self._me.mana += (self._me.mana * .03)
-        if self._targetNextToMe or self._targetDistance <= self._me.attack_range:
-            if self._me.get_type() == 'mage':
-                self._me.mana -= (self._me.mana * .05)
-            return None, None, self._target.get_id()
+
+        if self.enemyNextToMe or self.estimatedEnemyDistance <= self.me.attack_range:
+            return None, None, self.enemy.get_id()
         else:
             self.build_path_to_target()
-            if self._targetDistance > (self._me.move_range + self._me.attack_range):
-                self.set_ai_goal_position(self._me.move_range + math.floor(self._me.move_range * .5))
-                if self._me.get_type() == 'mage':
-                    self._me.mana += (self._me.mana * .03)
-                return self._goalX, self._goalY, None
+            if self.me.get_type() == 'knight':
+                if self.enemyPathDistance > (self.me.move_range + self.me.attack_range):
+                    self.set_ai_goal_position(self.me.move_range + math.floor(self.me.move_range * .5))
+                    return self._goalX, self._goalY, None
+                else:
+                    self.set_ai_goal_position(self.me.move_range)
+                    return self._goalX, self._goalY, self.enemy.get_id()
             else:
-                self.set_ai_goal_position(self._me.move_range)
-                if self._me.get_type() == 'mage':
-                    self._me.mana -= (self._me.mana * .05)
-                return self._goalX, self._goalY, self._target.get_id()
+                if self.enemyPathDistance > self.me.move_range:
+                    self.set_ai_goal_position(self.me.move_range + math.floor(self.me.move_range * .5))
+                    return self._goalX, self._goalY, None
+                else:
+                    self.set_ai_goal_position(self.me.move_range)
+                    return self._goalX, self._goalY, self.enemy.get_id()
